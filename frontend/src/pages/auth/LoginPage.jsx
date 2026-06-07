58e20a26993ab1fc451c9fd24e42e9ea979da79d
ahmickmontana <ahmickmontana@gmail.com>
2026-06-07 19:56:42 +1200

    Added back button in login page to improve UX

--- frontend/src/pages/auth/LoginPage.jsx
+++ frontend/src/pages/auth/LoginPage.jsx
@@ -38,62 +38,67 @@ export default function LoginPage() {
     }
     
     return (
-        <div className="login-page">
-            <div className="login-bg" />
-            <h1 className="page-title">
-                Triplana.
-            </h1>
-            <p className="page-subtitle">
-                Plan Your Trips Smarter.
-            </p>
+        <div>
+            <button className="back-btn" onClick={() => navigate('/')}>
+                    ⇦ Back
+            </button>
+            <div className="login-page">
+                <div className="login-bg" />
+                <h1 className="page-title">
+                    Triplana.
+                </h1>
+                <p className="page-subtitle">
+                    Plan Your Trips Smarter.
+                </p>
 
-            <h2 className="form-title">
-                Login
-            </h2>
+                <h2 className="form-title">
+                    Login
+                </h2>
 
-            <div className="login-form">
-                <div className="form-element">
-                    <label className="input-label">Email</label>
-                    <input
-                        type="text"
-                        placeholder="example@email.com"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        className={`input-field ${errors.email ? 'input-error-border' : ''}`}
-                    />
-                    {errors.email && <p className="input-error">{errors.email}</p>}
-                </div>
-
-                <div className="form-element">
-                    <label className="input-label">Password</label>
-                    <div className={`input-wrapper ${errors.password ? 'input-error-border' : ''}`}>
+                <div className="login-form">
+                    <div className="form-element">
+                        <label className="input-label">Email</label>
                         <input
-                            type={showPassword ? 'text' : 'password'}
-                            placeholder=""
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            className="input-field"
+                            type="text"
+                            placeholder="example@email.com"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className={`input-field ${errors.email ? 'input-error-border' : ''}`}
                         />
-                        <button 
-                            className="show-btn"
-                            onClick={() => setShowPassword(!showPassword)}
-                        >
-                            {showPassword ? 'Hide' : 'Show'}
-                        </button>
+                        {errors.email && <p className="input-error">{errors.email}</p>}
                     </div>
-                    {errors.password && <p className="input-error">{errors.password}</p>}
-                    {errors.message && <p className="input-error">{errors.message}</p>}
+
+                    <div className="form-element">
+                        <label className="input-label">Password</label>
+                        <div className={`input-wrapper ${errors.password ? 'input-error-border' : ''}`}>
+                            <input
+                                type={showPassword ? 'text' : 'password'}
+                                placeholder=""
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="input-field"
+                            />
+                            <button 
+                                className="show-btn"
+                                onClick={() => setShowPassword(!showPassword)}
+                            >
+                                {showPassword ? 'Hide' : 'Show'}
+                            </button>
+                        </div>
+                        {errors.password && <p className="input-error">{errors.password}</p>}
+                        {errors.message && <p className="input-error">{errors.message}</p>}
+                    </div>
+
+                    <a className="forgot-link" onClick={() => navigate('/forgot-password')}>
+                        Forgot password?
+                    </a>
+
+                    <button className="login-btn" onClick={handleSubmit}>
+                        Log in
+                    </button>
                 </div>
-
-                <a className="forgot-link" onClick={() => navigate('/forgot-password')}>
-                    Forgot password?
-                </a>
-
-                <button className="login-btn" onClick={handleSubmit}>
-                    Log in
-                </button>
+                
             </div>
-            
         </div>
     );
 }
