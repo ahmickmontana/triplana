9d501983cffccc7dc579f201d99d35b09b181c15
ahmickmontana <ahmickmontana@gmail.com>
2026-07-05 01:37:35 +1200

    Sprint 3 wrote integration tests for TripController class

--- backend/src/main/java/com/triplana/backend/repository/UserRepository.java
+++ backend/src/main/java/com/triplana/backend/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.triplana.backend.repository;
 
 import com.triplana.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
